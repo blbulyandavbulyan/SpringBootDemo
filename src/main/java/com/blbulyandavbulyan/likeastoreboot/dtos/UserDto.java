1d54fa274f15c3ae9db104aac02ec9688809ef15
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-22 19:36:29 +0400

    userId renamed to id

--- src/main/java/com/blbulyandavbulyan/likeastoreboot/dtos/UserDto.java
+++ src/main/java/com/blbulyandavbulyan/likeastoreboot/dtos/UserDto.java
@@ -2,7 +2,7 @@ package com.blbulyandavbulyan.likeastoreboot.dtos;
 
 import com.blbulyandavbulyan.likeastoreboot.entities.User;
 
-public record UserDto(Long userId, String name, Long age) {
+public record UserDto(Long id, String name, Long age) {
     public User toUser() {
         return new User(name, age);
     }
