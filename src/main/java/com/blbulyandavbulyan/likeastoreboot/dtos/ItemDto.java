c267deef0812cdf746bce052974d025cb76676f8
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-22 19:48:55 +0400

    Removed unused method

--- src/main/java/com/blbulyandavbulyan/likeastoreboot/dtos/ItemDto.java
+++ src/main/java/com/blbulyandavbulyan/likeastoreboot/dtos/ItemDto.java
@@ -7,7 +7,4 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 public record  ItemDto (@NotNull Long id, @NotEmpty String title, @NotNull @Min(1) Long price){
-    public Item toItem(){
-        return new Item(title, price);
-    }
 }
