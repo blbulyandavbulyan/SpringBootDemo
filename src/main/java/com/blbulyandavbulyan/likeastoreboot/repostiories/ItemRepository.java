58695fa89a37419a26bd3c59995053e1ceb44f81
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-22 19:49:30 +0400

    Added findAllBy method for finding dtos in ItemRepository

--- src/main/java/com/blbulyandavbulyan/likeastoreboot/repostiories/ItemRepository.java
+++ src/main/java/com/blbulyandavbulyan/likeastoreboot/repostiories/ItemRepository.java
@@ -4,6 +4,9 @@ import com.blbulyandavbulyan.likeastoreboot.entities.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    <T> Collection<T> findAllBy(Class<T> type);
 }
