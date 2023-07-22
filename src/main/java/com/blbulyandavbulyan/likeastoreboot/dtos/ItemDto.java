528d3e7151bc0d4e6b3b54386a15b1f8ae4a19b7
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-22 19:47:21 +0400

    Added annotation for validation

--- src/main/java/com/blbulyandavbulyan/likeastoreboot/dtos/ItemDto.java
+++ src/main/java/com/blbulyandavbulyan/likeastoreboot/dtos/ItemDto.java
@@ -1,7 +1,12 @@
 package com.blbulyandavbulyan.likeastoreboot.dtos;
 
 import com.blbulyandavbulyan.likeastoreboot.entities.Item;
-public record  ItemDto (Long id, String title, Long price){
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public record  ItemDto (@NotNull Long id, @NotEmpty String title, @NotNull @Min(1) Long price){
     public Item toItem(){
         return new Item(title, price);
     }
