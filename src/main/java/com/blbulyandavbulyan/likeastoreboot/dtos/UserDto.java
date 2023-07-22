39d9c58064b2eaa74d26bb7e662442d6939115b3
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-22 19:11:18 +0400

    Removed secondary constructor, because with two constructors records doesn't work

--- src/main/java/com/blbulyandavbulyan/likeastoreboot/dtos/UserDto.java
+++ src/main/java/com/blbulyandavbulyan/likeastoreboot/dtos/UserDto.java
@@ -3,7 +3,7 @@ package com.blbulyandavbulyan.likeastoreboot.dtos;
 import com.blbulyandavbulyan.likeastoreboot.entities.User;
 
 public record UserDto(Long userId, String name, Long age) {
-    public UserDto(User user){
-        this(user.getId(), user.getName(), user.getAge());
+    public User toUser() {
+        return new User(name, age);
     }
 }
