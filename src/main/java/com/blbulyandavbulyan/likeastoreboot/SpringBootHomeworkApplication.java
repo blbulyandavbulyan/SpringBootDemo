31a61365eda897dfc76f2b8dd7bf5e475871be41
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-22 17:47:08 +0400

    Initial commit

---
+++ src/main/java/com/blbulyandavbulyan/likeastoreboot/SpringBootHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.blbulyandavbulyan.likeastoreboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHomeworkApplication.class, args);
+	}
+
+}
