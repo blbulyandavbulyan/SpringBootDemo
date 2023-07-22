0b4db2c375e67d2a19fe50a7a506362f2694ee16
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-22 19:28:18 +0400

    Added UserAddRequest

---
+++ src/main/java/com/blbulyandavbulyan/likeastoreboot/dtos/UserAddRequest.java
@@ -0,0 +1,8 @@
+package com.blbulyandavbulyan.likeastoreboot.dtos;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public record UserAddRequest(@NotEmpty String name, @NotNull @Min(1) Long age) {
+}
