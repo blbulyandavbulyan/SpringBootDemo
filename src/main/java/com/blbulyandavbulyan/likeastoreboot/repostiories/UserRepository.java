7f2c58ac57e5b6fafd05cb16b2c6857294fcf497
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-22 19:36:44 +0400

    findAllBy for dtos method added

--- src/main/java/com/blbulyandavbulyan/likeastoreboot/repostiories/UserRepository.java
+++ src/main/java/com/blbulyandavbulyan/likeastoreboot/repostiories/UserRepository.java
@@ -4,6 +4,9 @@ import com.blbulyandavbulyan.likeastoreboot.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    <T>Collection<T> findAllBy(Class<T> type);
 }
