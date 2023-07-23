6251ad863febac857d3914225f12fc3360813eae
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-23 10:58:16 +0400

    Added updateItem method in ItemRepository

--- src/main/java/com/blbulyandavbulyan/likeastoreboot/repostiories/ItemRepository.java
+++ src/main/java/com/blbulyandavbulyan/likeastoreboot/repostiories/ItemRepository.java
@@ -1,8 +1,14 @@
 package com.blbulyandavbulyan.likeastoreboot.repostiories;
 
+import com.blbulyandavbulyan.likeastoreboot.dtos.ItemDto;
+import com.blbulyandavbulyan.likeastoreboot.dtos.UserDto;
 import com.blbulyandavbulyan.likeastoreboot.entities.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 
@@ -10,4 +16,8 @@ import java.util.Collection;
 public interface ItemRepository extends JpaRepository<Item, Long> {
     <T> Collection<T> findAllBy(Class<T> type);
     <T> T findById(Long id, Class<T> type);
+    @Transactional
+    @Modifying
+    @Query("UPDATE Item i SET i.title = :#{#itemDto.title}, i.price = :#{#itemDto.price} WHERE i.id = :#{#itemDto.id}")
+    void updateItem(@Param("itemDto") ItemDto itemDto);
 }
